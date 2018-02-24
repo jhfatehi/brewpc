4dbbb1d52054f80880abc4b3d3010ce126c09af8
Kenneth J. Sauer <31974927+kjsauer@users.noreply.github.com>
2018-02-24 11:48:17 -0800

    cosmetic only
    non-material changes, no change to functionality

--- input2/source/dbwrite.py
+++ input2/source/dbwrite.py
@@ -4,7 +4,7 @@ def test(db_path, brew_num, batch_num, data1, data2, data3):
 	conn = sqlite3.connect(db_path)
 	c = conn.cursor()
 	c.execute('''UPDATE brew
-				 SET data1 = ?, data2 = ?, data3 = ?
-				 WHERE brew_num = ? AND batch = ?''', (data1, data2, data3, brew_num, batch_num))
+           SET data1 = ?, data2 = ?, data3 = ?
+           WHERE brew_num = ? AND batch = ?''', (data1, data2, data3, brew_num, batch_num))
 	conn.commit()
 	conn.close()
