10cfa975148ddb58221fe73fc6a26285615a564d
Your Name <you@example.com>
2018-03-12 21:55:19 -0700

    todo 3 - modify db for mash

---
+++ input2/add_process.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+size = raw_input('Size? ')
+brand = raw_input('Brand? ')
+tGRStemp = raw_input('Grist Temp? ')
+tSTKtemp = raw_input('Strike Temp? ')
+tMSHvol = raw_input('Mash Volume? ')
+tMSHtemp = raw_input('Mash Temperature? ')
+tMASHphLOW = raw_input('Low Mash pH? ')
+tMASHphHI = raw_input('High Mash pH? ')
+tSPGvol = raw_input('Sparge Volume? ')
+
+conn = sqlite3.connect('input2_test2.db')
+c = conn.cursor()
+c.execute('INSERT INTO process VALUES (?,?,?,?,?,?,?,?,?)', (size,
+															brand,
+															tGRStemp,
+															tSTKtemp,
+															tMSHvol,
+															tMSHtemp,
+															tMASHphLOW,
+															tMASHphHI,
+															tSPGvol))
+conn.commit()
+conn.close()
--- input2/input2_db.py
+++ input2/input2_db.py
@@ -1,8 +1,49 @@
 import sqlite3
 
-conn = sqlite3.connect('input2_test.db')
+conn = sqlite3.connect('input2_test2.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE brew
              (brew_num, batch, size, brand, data1, data2, data3)''')
+
+c.execute('''CREATE TABLE mash (
+				brew_num,
+				batch,
+				size,
+				brand,
+				dGRStemp,
+				dSTKtemp,
+				dMSHvol,
+				dMSHtemp,
+				dMSHtime,
+				dBREWsig,
+				dRNCvol,
+				dVLFtime,
+				dMASHph,
+				d1RNvol,
+				dSPGvol,
+				dROFtime,
+				dRACKcnt,
+				dFILLtime,
+				dFILLvol,
+				tGRStemp,
+				tSTKtemp,
+				tMSHvol,
+				tMSHtemp,
+				tMASHphLOW,
+				tMASHphHI,
+				tSPGvol
+				)''')
+
+c.execute('''CREATE TABLE process (
+				size,
+				brand,
+				tGRStemp,
+				tSTKtemp,
+				tMSHvol,
+				tMSHtemp,
+				tMASHphLOW,
+				tMASHphHI,
+				tSPGvol
+				)''')
 conn.commit()
 conn.close()
