b2c57998714bafd197b2580d2e18a2724108303a
Kenneth J. Sauer <31974927+kjsauer@users.noreply.github.com>
2018-03-16 19:30:57 -0700

    fix str bug and add mash function
    convert ii+1 to string; add function to insert values to mash table row for a given brew_num and batch

--- input2/source/dbwrite.py
+++ input2/source/dbwrite.py
@@ -15,11 +15,68 @@ def add_brew(db_path, batches, brew_num, brew_size, brand):
     brew_data = []
     mash_data = []
     for ii in range(int(batches)):
-        brew_data.append((brew_num, ii+1, brew_size, brand, n,n,n)) #for test sheet.  remmove latter
-        mash_data.append((brew_num, ii+1, brew_size, brand, n,n,n,n,n,n,n,n,n,n,n,n,n,n,n))
+        brew_data.append((brew_num, str(ii+1), brew_size, brand, n,n,n)) #for test sheet.  remmove latter
+        mash_data.append((brew_num, str(ii+1), brew_size, brand, n,n,n,n,n,n,n,n,n,n,n,n,n,n,n))
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.executemany('insert into brew values (?,?,?,?,?,?,?)', brew_data) #for test sheet.  remmove latter
     c.executemany('insert into mash values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', mash_data)
+    conn.commit()
+    conn.close()
+
+def mash(db_path,
+    brew_num,
+    batch_num,
+    dGRStemp,
+    dSTKtemp,
+    dMSHvol,
+    dMSHtemp,
+    dMSHtime,
+    dBREWsig,
+    dRNCvol,
+    dVLFtime,
+    dMASHph,
+    d1RNvol,
+    dSPGvol,
+    dROFtime,
+    dRACKcnt,
+    dFILLtime,
+    dFILLvol):
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('''UPDATE mash
+           SET dGRStemp = ?,
+           dSTKtemp = ?,
+           dMSHvol = ?,
+           dMSHtemp = ?,
+           dMSHtime = ?,
+           dBREWsig = ?,
+           dRNCvol = ?,
+           dVLFtime = ?,
+           dMASHph = ?,
+           d1RNvol = ?,
+           dSPGvol = ?,
+           dROFtime = ?,
+           dRACKcnt = ?,
+           dFILLtime = ?,
+           dFILLvol = ?
+           WHERE brew_num = ? AND batch = ?''',
+           (dGRStemp,
+            dSTKtemp,
+            dMSHvol,
+            dMSHtemp,
+            dMSHtime,
+            dBREWsig,
+            dRNCvol,
+            dVLFtime,
+            dMASHph,
+            d1RNvol,
+            dSPGvol,
+            dROFtime,
+            dRACKcnt,
+            dFILLtime,
+            dFILLvol,
+            brew_num,
+            batch_num))
     conn.commit()
     conn.close()
