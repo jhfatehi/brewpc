9366b1bde1a0bf54a28def31c845a525acae7b98
Your Name <you@example.com>
2018-04-06 21:29:12 -0700

    rev1 cleanup

--- input2/input2_db.py
+++ input2/input2_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-db_path = 'input2_test4.db'
+db_path = 'brewpc_database_v1.db'
 
 conn = sqlite3.connect(db_path)
 c = conn.cursor()
@@ -69,43 +69,43 @@ c.execute('INSERT INTO process VALUES (?,?,?,?,?,?,?,?,?)', (size,
 															tMASHphHI,
 															tSPGvol))
 
-size = '11'
-brand = '11111111111'
-tGRStemp = '11'
-tSTKtemp = '111'
-tMSHvol = '111'
-tMSHtemp = '111'
-tMASHphLOW = '1.1'
-tMASHphHI = '1.1'
-tSPGvol = '1'
-c.execute('INSERT INTO process VALUES (?,?,?,?,?,?,?,?,?)', (size,
-															brand,
-															tGRStemp,
-															tSTKtemp,
-															tMSHvol,
-															tMSHtemp,
-															tMASHphLOW,
-															tMASHphHI,
-															tSPGvol))
+# size = '11'
+# brand = '11111111111'
+# tGRStemp = '11'
+# tSTKtemp = '111'
+# tMSHvol = '111'
+# tMSHtemp = '111'
+# tMASHphLOW = '1.1'
+# tMASHphHI = '1.1'
+# tSPGvol = '1'
+# c.execute('INSERT INTO process VALUES (?,?,?,?,?,?,?,?,?)', (size,
+# 															brand,
+# 															tGRStemp,
+# 															tSTKtemp,
+# 															tMSHvol,
+# 															tMSHtemp,
+# 															tMASHphLOW,
+# 															tMASHphHI,
+# 															tSPGvol))
 
-size = '22'
-brand = '222222222222'
-tGRStemp = '22'
-tSTKtemp = '222'
-tMSHvol = '222'	
-tMSHtemp = '222'
-tMASHphLOW = '2.2'
-tMASHphHI = '2.2'
-tSPGvol = '222'
-c.execute('INSERT INTO process VALUES (?,?,?,?,?,?,?,?,?)', (size,
-															brand,
-															tGRStemp,
-															tSTKtemp,
-															tMSHvol,
-															tMSHtemp,
-															tMASHphLOW,
-															tMASHphHI,
-															tSPGvol))
+# size = '22'
+# brand = '222222222222'
+# tGRStemp = '22'
+# tSTKtemp = '222'
+# tMSHvol = '222'	
+# tMSHtemp = '222'
+# tMASHphLOW = '2.2'
+# tMASHphHI = '2.2'
+# tSPGvol = '222'
+# c.execute('INSERT INTO process VALUES (?,?,?,?,?,?,?,?,?)', (size,
+# 															brand,
+# 															tGRStemp,
+# 															tSTKtemp,
+# 															tMSHvol,
+# 															tMSHtemp,
+# 															tMASHphLOW,
+# 															tMASHphHI,
+# 															tSPGvol))
 	
 conn.commit()
 conn.close()
